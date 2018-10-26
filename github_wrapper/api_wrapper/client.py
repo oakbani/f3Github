91019aa57d0bed50f6fdaeea8740235bb498d3f1
Mariamjamal32 <mariamjamal32@gmail.com>
2018-10-26 15:58:39 +0500

    Lint warnings and errors fixed

--- github_wrapper/api_wrapper/client.py
+++ github_wrapper/api_wrapper/client.py
@@ -1,22 +1,30 @@
 import gwrapper
 
-request = gwrapper.GWrapper("https://api.github.com/repos/oakbani/f3Github/pulls", username='', pwd='')
+request = gwrapper.GWrapper(
+    "https://api.github.com/repos/oakbani/f3Github/pulls",
+    username='',
+    pwd='')
+
 
 def get_all_pulls(**params):
     request.list_pulls(params)
 
+
 def get_pull(id):
     request.list_pull(id)
 
+
 def get_pull_commits(pull_id):
-    #global request
     request.list_pull_commits(pull_id)
+
 
 def get_pull_files(pull_id):
     request.list_pull_files(pull_id)
 
+
 def add_pull_request(**params):
     request.post_pull_request(params)
+
 
 def edit_pull_request(pull_id, **params):
     request.update_pull_request(pull_id, params)
