b1ac9f1b1459a1bed8f505f2aaa5907d2bb45641
Mariamjamal32 <mariamjamal32@gmail.com>
2018-11-08 12:02:17 +0500

    feat(logger.py): Add logging feature

--- client.py
+++ client.py
@@ -1,17 +1,45 @@
+from gwrapper import logger_interface
 from gwrapper import wrapper
+import logging
+
+
+# request = wrapper.GWrapper(
+#     '''{
+#         "url": ["https://api.github.com/repos/oakbani/f3Github"],
+#         "credentials":[
+#             {
+#                 "username": "1234",
+#                 "pwd": "1234"
+#             }
+#         ],
+#         "version": 2
+#     }'''
+# )
+
+class ClientLogger(logger_interface.ExampleLogger):
+    def __init__(
+        self,
+        handler=logging.StreamHandler(),
+        logger_name=__name__,
+        min_level=logging.INFO
+    ):
+        self.logger = logging.getLogger(logger_name)
+        self.logger.setLevel(min_level)
+        self.logger.handlers = [handler]
+        self.min_level = min_level
+
+    def log(self, lvl, msg):
+        self.logger.log(lvl, msg)
+
 
 request = wrapper.GWrapper(
     '''{
-        "url": ["https://api.github.com/repos/oakbani/f3Github"],
-        "credentials":[
-            {
-                "username": "1234",
-                "pwd": "1234"
-            }
-        ],
-
-        "version": 2
-    }'''
+        "url": "https://api.github.com/repos/oakbani/f3Github",
+        "username": "MariamJamal32",
+        "pwd": "Mariam1374",
+        "version": 1
+    }''',
+    logger=ClientLogger()
 )
 
 
@@ -31,21 +59,14 @@ def get_pull_files(pull_id):
     request.list_pull_files(pull_id)
 
 
-def add_pull_request(**params):
-    request.post_pull_request(params)
-
-
-def edit_pull_request(pull_id, **params):
-    request.update_pull_request(pull_id, params)
-
-
 def get_pr_by_commits():
-    request.get_pr_with_num_of_commits(100, 'lt', state="closed")
+    request.get_pr_with_num_of_commits(2, 'gt', state="closed")
 
 
 def get_pr_by_files():
     request.get_pr_with_num_of_files(80, 'lt', state="closed")
 
 
+get_pr_by_commits()
 # request.get_pr_by_commit_text(['check', 'abc'], 'all', state="closed")
 # request.get_pr_by_file_name(['.gitignore', 'green'], 'all', state='closed')
--- gwrapper/wrapper.py
+++ gwrapper/wrapper.py
@@ -1,15 +1,17 @@
 import fastjsonschema
+from gwrapper import logger_interface
 from gwrapper.number_filter import Filter
 from gwrapper.string_filter import String_Filter
 import json
+import logging
 import requests
 
 
 class GWrapper(object):
-    def __new__(cls, json_init):
+    def __new__(cls, json_init, logger=None):
+
         with open('gwrapper/schemas/wrapper_schema.json') as json_data:
             schema = json.load(json_data)
-
         try:
             validate = fastjsonschema.compile(schema)
             validate(json.loads(json_init))
@@ -25,7 +27,9 @@ class GWrapper(object):
             print("No repository exists with url: ", url)
             return None
 
-    def __init__(self, json_init):
+    def __init__(self, json_init, logger=None):
+        self.logger = logger or logger_interface.NoOpLogger()
+        self.level = logger.min_level
         self.url = json.loads(json_init)['url']+'/pulls'
         self.auth = (
             json.loads(json_init)['username'],
@@ -35,19 +39,28 @@ class GWrapper(object):
     # to list all pull requests made to a repos
     # params: base, state, sort, head, direction
     def list_pulls(self, params={}):
-        return requests.get(self.url, params=params, auth=self.auth)
+        response = requests.get(self.url, params=params, auth=self.auth)
+        msg = str(len(response.json())) + ' pull requests fetched'
+        self.logger.log(logging.INFO, msg)
+        return response
 
     # get pull requests by number of commits
     def get_pr_with_num_of_commits(self, num, filter, **params):
         pull_requests = self.list_pulls(params).json()
         f = Filter(pull_requests, num, filter, 'commits', self.auth)
-        return print(f.filter_by_number())
+        response = f.filter_by_number()
+        msg = str(len(response)) + ' pull requests found with commit filter'
+        self.logger.log(logging.INFO, msg)
+        return print(response)
 
     # get pull requests by number of files changed
     def get_pr_with_num_of_files(self, num, filter, **params):
         pull_requests = self.list_pulls(params).json()
         f = Filter(pull_requests, num, filter, 'files', self.auth)
-        return print(f.filter_by_number())
+        response = f.filter_by_number()
+        msg = str(len(response)) + ' pull requests found with file filter'
+        self.logger.log(logging.INFO, msg)
+        return print(response)
 
     # get pull requests by commit message keywords
     def get_pr_by_commit_text(self, text_list, filter, **params):
@@ -55,7 +68,10 @@ class GWrapper(object):
         f = String_Filter(
             pull_requests, text_list, filter, 'commits', self.auth
         )
-        return print(f.filter_by_string())
+        response = f.filter_by_string()
+        msg = str(len(response)) + ' pull requests found with commit text filter'
+        self.logger.log(logging.INFO, msg)
+        return print(response)
 
     # get pull requests by file name keywords
     def get_pr_by_file_name(self, name_list, filter, **params):
@@ -63,4 +79,7 @@ class GWrapper(object):
         f = String_Filter(
             pull_requests, name_list, filter, 'files', self.auth
         )
-        return print(f.filter_by_string())
+        response = f.filter_by_string()
+        msg = str(len(response)) + ' pull requests found with file name filter'
+        self.logger.log(logging.INFO, msg)
+        return print(response)
