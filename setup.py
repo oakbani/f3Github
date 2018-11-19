9e086f2da21e7f683433f7a09e0dc31dfdd6fb5a
Mariamjamal32 <mariamjamal32@gmail.com>
2018-11-19 16:42:01 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name="f3Github",
+    name="github-pr-utility",
     version="0.1",
     author="Mariam Jamal",
     author_email="mjamal@folio3.com",
@@ -10,11 +10,17 @@ setup(
     long_description="Package to provide find pull requests for Github repos"
                      "with commit and file filters",
     long_description_content_type="text/markdown",
-    url="https://github.com/oakbani/f3Github",
-    packages=find_packages(),
+    url="https://github.com/oakbani/f3Github/gwrapper",
+    packages=find_packages(
+      exclude=['tests']
+    ),
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5.5',
+        'Programming Language :: Python :: 3.6'
     ],
-    test_suite='f3Github.tests',
+    test_suite='tests',
 )
